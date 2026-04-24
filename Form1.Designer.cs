be48f7064215504bd105bc68ad5ea80ed0ffc564
ComwpODG <comwp6@gmail.com>
2026-04-24 04:04:59 -0500

    Uploaded project from offline workflow

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ScriptScripter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPickTextFile = new System.Windows.Forms.Button();
+            this.btnPickDestFolder = new System.Windows.Forms.Button();
+            this.lblClarity = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "select your txt file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(369, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Batch File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPickTextFile
+            // 
+            this.btnPickTextFile.Location = new System.Drawing.Point(12, 12);
+            this.btnPickTextFile.Name = "btnPickTextFile";
+            this.btnPickTextFile.Size = new System.Drawing.Size(189, 23);
+            this.btnPickTextFile.TabIndex = 4;
+            this.btnPickTextFile.Text = "Select Text File";
+            this.btnPickTextFile.UseVisualStyleBackColor = true;
+            this.btnPickTextFile.Click += new System.EventHandler(this.btnPickTextFile_Click);
+            // 
+            // btnPickDestFolder
+            // 
+            this.btnPickDestFolder.Location = new System.Drawing.Point(207, 12);
+            this.btnPickDestFolder.Name = "btnPickDestFolder";
+            this.btnPickDestFolder.Size = new System.Drawing.Size(174, 23);
+            this.btnPickDestFolder.TabIndex = 5;
+            this.btnPickDestFolder.Text = "Select Destination Folder";
+            this.btnPickDestFolder.UseVisualStyleBackColor = true;
+            this.btnPickDestFolder.Click += new System.EventHandler(this.btnPickDestFolder_Click);
+            // 
+            // lblClarity
+            // 
+            this.lblClarity.Location = new System.Drawing.Point(9, 51);
+            this.lblClarity.Name = "lblClarity";
+            this.lblClarity.Size = new System.Drawing.Size(372, 94);
+            this.lblClarity.TabIndex = 6;
+            this.lblClarity.Text = "Select a text file and a destination folder.";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "select your txt file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 194);
+            this.Controls.Add(this.lblClarity);
+            this.Controls.Add(this.btnPickDestFolder);
+            this.Controls.Add(this.btnPickTextFile);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Script Scripter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPickTextFile;
+        private System.Windows.Forms.Button btnPickDestFolder;
+        private System.Windows.Forms.Label lblClarity;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ScriptScripter
+{
+    public partial class Form1 : Form
+    {
+        private string inputFilePath = "";
+        private string outputFolderPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Pick input text file
+        private void btnPickTextFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                inputFilePath = openFileDialog1.FileName;
+                UpdateClarity();
+            }
+        }
+
+        // Pick destination folder
+        private void btnPickDestFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputFolderPath = folderDialog.SelectedPath;
+                    UpdateClarity();
+                }
+            }
+        }
+
+        // Main generate button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFilePath) || string.IsNullOrEmpty(outputFolderPath))
+            {
+                MessageBox.Show("Please select both an input file and an output folder.");
+                return;
+            }
+
+            try
+            {
+                string inputText = File.ReadAllText(inputFilePath);
+                inputText = Sanitize(inputText);
+
+                string outputFileName = Path.GetFileNameWithoutExtension(inputFilePath) + "_script.vbs";
+                string fullOutputPath = Path.Combine(outputFolderPath, outputFileName);
+
+                using (StreamWriter sw = new StreamWriter(fullOutputPath, false))
+                {
+                    WriteHeader(sw);
+                    WriteChunks(sw, inputText, 5, 15);
+                }
+
+                MessageBox.Show("Script generated successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Updates the "promise" label
+        private void UpdateClarity()
+        {
+            if (string.IsNullOrEmpty(inputFilePath) && string.IsNullOrEmpty(outputFolderPath))
+            {
+                lblClarity.Text = "Select a text file and destination folder.";
+                return;
+            }
+
+            else if (string.IsNullOrEmpty(outputFolderPath))
+            {
+                lblClarity.Text = "Select a destination folder.";
+                return;
+            }
+            else if (string.IsNullOrEmpty(inputFilePath))
+            {
+                lblClarity.Text = "Select a text file.";
+                return;
+            }
+
+
+            string fileName = Path.GetFileNameWithoutExtension(inputFilePath) + "_script.vbs";
+
+            lblClarity.Text =
+                $"The program will generate \"{fileName}\" at \"{outputFolderPath}\" " +
+                $"containing the contents of \"{Path.GetFileName(inputFilePath)}\" as a script of keystrokes.";
+        }
+
+        private string Sanitize(string input)
+        {
+            return input
+                .Replace(Environment.NewLine, " ")
+                .Replace("\"", "'")
+                .Replace("{", "-")
+                .Replace("}", "-");
+        }
+
+        private void WriteHeader(StreamWriter sw)
+        {
+            sw.WriteLine("Set WshShell = WScript.CreateObject(\"WScript.Shell\")");
+            sw.WriteLine("Delay = 15");
+            sw.WriteLine($"WScript.Sleep 2000");
+        }
+
+        private void WriteChunks(StreamWriter sw, string text, int chunkSize, int delay)
+        {
+            int index = 0;
+
+            while (index < text.Length)
+            {
+                sw.WriteLine("WshShell.SendKeys \"{ENTER}\"");
+                sw.WriteLine($"WScript.Sleep {delay}");
+
+                for (int i = 0; i < 6 && index < text.Length; i++)
+                {
+                    int remaining = text.Length - index;
+                    int take = Math.Min(chunkSize, remaining);
+
+                    string chunk = text.Substring(index, take);
+
+                    sw.WriteLine($"WshShell.SendKeys \"{chunk}\"");
+                    sw.WriteLine($"WScript.Sleep {delay}");
+
+                    index += take;
+                }
+            }
+        }
+    }
+}
